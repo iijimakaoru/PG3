1392954bbe5268e8d4f728cc73868b34f407e634
IijimaKaoru <k021g1465@g.neec.ac.jp>
2023-01-23 10:57:05 +0900

    2-1多分できた

--- PG3_kadai/main.cpp
+++ PG3_kadai/main.cpp
@@ -1,22 +1,71 @@
-#include <stdio.h>
-#include <Windows.h>
-#include <time.h>
-#include <stdlib.h>
-#include <functional>
+#include <iostream>
+#include <vector>
 #include <list>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-	list<const char*> name{ "oosaki","gotanda","meguro","ebisu","sibuya","harazyuku","yoyogi","sinzyuku","sinookubo","takadanobaba","meziro","ikebukuro","ootuka", "sugamo",
-	"komagome","tabata","nippori","uguisudani","ueno","okatimati","akihabara","kanda","toukyou","yuurakutyou","sinbasi","hamamatutyou","tamati","sinagawa"};
-
-	for (int i = 0; i < name.size(); i++)
+	// 山手線
+	list<const char*> name
 	{
-		printf("%s\n", name);
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanohara",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatutsucho",
+		"Shimbasi",
+		"Yurakucho"
+	};
+
+	// 1970
+	printf("1970年\n");
+	for (auto itr = name.begin(); itr != name.end(); ++itr)
+	{
+		cout << *itr << "\n";
 	}
+	printf("\n");
+
+	// 2019
+	printf("2019年\n");
+	list<const char*>::iterator name1970 = next(name.begin(), 7);
+	name.insert(name1970, "Nishi-Nippori");
+	for (auto itr = name.begin(); itr != name.end(); ++itr)
+	{
+		cout << *itr << "\n";
+	}
+	printf("\n");
+
+	// 2022
+	printf("2022年\n");
+	list<const char*>::iterator name2019 = next(name.begin(), 25);
+	name.insert(name2019, "TakanawaGateWay");
+	for (auto itr = name.begin(); itr != name.end(); ++itr)
+	{
+		cout << *itr << "\n";
+	}
+	printf("\n");
 
 	return 0;
 }
